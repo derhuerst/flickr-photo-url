1717888a415ebce1dff438e5356c679b7a5259ea
Jannis R <mail@jannisr.de>
2016-05-18 15:01:45 +0200

    always return a promise

--- index.js
+++ index.js
@@ -8,17 +8,18 @@ const agent = 'derhuerst/flickr-photo-url'
 const sizes = ['sq', 'q', 't', 's', 'n', 'm', 'z', 'c', 'l', 'h', 'k', 'o']
 sizes.original = 'o'
 
-const url = (user, id, size) => {
-	if ('string' !== typeof user) throw new Error('invalid user')
-	if ('number' !== typeof id) throw new Error('invalid photo id')
+const url = (user, id, size) => new Promise((yay, nay) => {
+	if ('string' !== typeof user) return nay(new Error('invalid user'))
+	if ('number' !== typeof id) return nay(new Error('invalid photo id'))
 	if ('number' === typeof size) size = sizes[size] || sizes.original
 	else if ('string' === typeof size && sizes.indexOf(size) < 0)
-		throw new Error('invalid size')
+		return nay(new Error('invalid size'))
 
 	return got(`https://www.flickr.com/photos/${user}/${id}/sizes/${size}/`,
 		{headers: {'user-agent': agent}})
-	.catch((err) => err)
-	.then((res) => cheerio.load(res.body)('#allsizes-photo img').attr('src'))
-}
+	.then((res) =>
+		cheerio.load(res.body)('#allsizes-photo img').attr('src'), nay)
+	.then(yay, nay)
+})
 
 module.exports = Object.assign(url, {sizes})
