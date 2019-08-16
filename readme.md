1b213d7020c0813cc509666a01f09949053144ad
Dario Vladovic <d.vladimyr@gmail.com>
2019-08-16 14:53:10 +0200

    Configure ESLint (standard config)
    - updating travis configuration (disable linting on versions
      bellow node@8 due to ESLint dropping support)
    - adding code style badge to readme

--- readme.md
+++ readme.md
@@ -8,6 +8,7 @@ In opposition to [`flickr-photo-urls`](https://github.com/npm-flickr/flickr-phot
 [![build status](https://img.shields.io/travis/derhuerst/flickr-photo-url.svg)](https://travis-ci.org/derhuerst/flickr-photo-url)
 [![dependency status](https://img.shields.io/david/derhuerst/flickr-photo-url.svg)](https://david-dm.org/derhuerst/flickr-photo-url)
 [![dev dependency status](https://img.shields.io/david/dev/derhuerst/flickr-photo-url.svg)](https://david-dm.org/derhuerst/flickr-photo-url#info=devDependencies)
+[![js-standard-style](https://img.shields.io/badge/code%20style-standard-brightgreen.svg)](http://standardjs.com)
 ![ISC-licensed](https://img.shields.io/github/license/derhuerst/flickr-photo-url.svg)
 [![chat on gitter](https://badges.gitter.im/derhuerst.svg)](https://gitter.im/derhuerst)
 [![support me on Patreon](https://img.shields.io/badge/support%20me-on%20patreon-fa7664.svg)](https://patreon.com/derhuerst)
@@ -30,7 +31,7 @@ url(user, photoId, [size])
 const url = require('flickr-photo-url')
 
 url('gilad_rom', 24148019753, url.sizes.original)
-.catch(console.error).then(console.log)
+  .catch(console.error).then(console.log)
 // -> https://c2.staticflickr.com/2/1534/24148019753_bc3bb60f50_o.jpg
 ```
 
--- test.js
+++ test.js
@@ -3,11 +3,9 @@
 const assert = require('assert')
 const test = require('tape-co').default
 const Promise = require('pinkie-promise')
-const fetch = require('fetch-ponyfill')({Promise: Promise}).fetch
+const { fetch } = require('fetch-ponyfill')(Promise)
 
-const url  = require('.')
-
-
+const url = require('.')
 
 const validUrl = (url) => {
 	return fetch(url, {
@@ -17,26 +15,22 @@ const validUrl = (url) => {
 			'user-agent': 'derhuerst/flickr-photo-url test'
 		}
 	})
-	.then((res) => {
-		assert(200 <= res.status, 'non-2xx status code')
-		assert(res.status < 300, 'non-2xx status code')
-		const type = res.headers.get('content-type').substr(0, 5)
-		assert.strictEqual(type, 'image', 'non-image mime type')
+		.then((res) => {
+			assert(res.status >= 200, 'non-2xx status code')
+			assert(res.status < 300, 'non-2xx status code')
+			const type = res.headers.get('content-type').substr(0, 5)
+			assert.strictEqual(type, 'image', 'non-image mime type')
 
-		return true // todo: abort res
-	})
+			return true // todo: abort res
+		})
 }
 
-
-
-test('works', function* (t) {
+test('works', function * (t) {
 	const original = yield url('gilad_rom', 24148019753)
 	t.ok(yield validUrl(original), 'valid url')
 })
 
-
-
-test('works with size `m`', function* (t) {
+test('works with size `m`', function * (t) {
 	const res = yield Promise.all([
 		url('gilad_rom', 24148019753),
 		url('gilad_rom', 24148019753, 'm')
@@ -48,9 +42,7 @@ test('works with size `m`', function* (t) {
 	t.ok(yield validUrl(medium), 'valid url')
 })
 
-
-
-test('works with size `small`', function* (t) {
+test('works with size `small`', function * (t) {
 	const res = yield Promise.all([
 		url('gilad_rom', 24148019753),
 		url('gilad_rom', 24148019753, url.sizes.small)
