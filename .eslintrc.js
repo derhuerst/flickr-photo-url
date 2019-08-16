7750a4e41c2ba70a83ea6bb93723f2406deaeb11
Dario Vladovic <d.vladimyr@gmail.com>
2019-08-16 14:53:10 +0200

    Configure ESLint (standard config)
    - updating travis configuration (disable linting on versions
      bellow node@8 due to ESLint dropping support)
    - adding code style badge to readme

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+	extends: 'standard',
+	rules: {
+		'no-tabs': ['off'],
+		indent: ['error', 'tab']
+	}
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 'use strict'
 
 const Promise = require('pinkie-promise')
-const {fetch} = require('fetch-ponyfill')({Promise})
-const {DomHandler, DomUtils, Parser} = require('htmlparser2')
+const { fetch } = require('fetch-ponyfill')({ Promise })
+const { DomHandler, DomUtils, Parser } = require('htmlparser2')
 
 const agent = 'derhuerst/flickr-photo-url'
 
@@ -14,41 +14,42 @@ sizes.small = 'n'
 sizes.square = 'q'
 
 const hasProp = (o, p) => Object.prototype.hasOwnProperty.call(o, p)
+const isString = arg => typeof arg === 'string'
+const isNumber = arg => typeof arg === 'number' || typeof arg === 'bigint'
 
 const url = (user, id, s) => {
-	if ('string' !== typeof user) return Promise.reject(new Error('invalid user'))
-	if ('number' !== typeof id) return Promise.reject(new Error('invalid photo id'))
-	if ('number' === typeof s) s = sizes[s] || sizes.original
-	else if ('string' === typeof s && sizes.indexOf(s) < 0 && !hasProp(sizes, s)) {
-		console.error(sizes.indexOf(s) < 0, !hasProp(sizes, s), s)
+	if (!isString(user)) return Promise.reject(new Error('invalid user'))
+	if (!isNumber(id)) return Promise.reject(new Error('invalid photo id'))
+	if (isNumber(s)) s = sizes[s] || sizes.original
+	else if (isString(s) && !sizes.includes(s) && !hasProp(sizes, s)) {
+		console.error(!sizes.includes(s), !hasProp(sizes, s), s)
 		return Promise.reject(new Error('invalid size'))
 	}
 
 	return fetch(`https://www.flickr.com/photos/${user}/${id}/sizes/${s}/`, {
 		redirect: 'follow',
-		headers: {'user-agent': agent}
+		headers: { 'user-agent': agent }
 	})
-	.then((res) => {
-		if (!res.ok) {
-			const err = new Error(res.statusText)
-			err.statusCode = res.status
-			throw err
-		}
-		return res.text()
-	})
-	.then((html) => {
-		const dom = parseHtml(html)
-		const container = DomUtils.getElementById('allsizes-photo', dom, true)
-		const img = DomUtils.findOne(el => el.tagName === 'img', [container], true)
-		const url = img.attribs.src
-		if (!url) throw new Error('failed to extract the URL from the Flickr page')
-		return url
-	})
+		.then((res) => {
+			if (!res.ok) {
+				const err = new Error(res.statusText)
+				err.statusCode = res.status
+				throw err
+			}
+			return res.text()
+		})
+		.then((html) => {
+			const dom = parseHtml(html)
+			const container = DomUtils.getElementById('allsizes-photo', dom, true)
+			const img = DomUtils.findOne(el => el.tagName === 'img', [container], true)
+			const url = img.attribs.src
+			if (!url) throw new Error('failed to extract the URL from the Flickr page')
+			return url
+		})
 }
 
-module.exports = Object.assign(url, {sizes})
+module.exports = Object.assign(url, { sizes })
 
-function parseHtml(html, options = {}) {
 function parseHtml (html, options = {}) {
 	const handler = new DomHandler(null, options)
 	new Parser(handler, options).end(html)
